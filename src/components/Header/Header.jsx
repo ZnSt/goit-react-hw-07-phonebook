a978b7f1c721656c96ddc7da6cfded3c3a55ccc6
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-03-25 13:05:45 +0200

    delete icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import { BsTelephone } from "react-icons/bs";
-
-import { HeaderSt, Title } from './Header.styled'
+import React from 'react';
+import { HeaderSt, Title } from './Header.styled';
 
 export const Header = () => {
   return (
     <HeaderSt>
-        <BsTelephone/>
-        <Title>Call me, man</Title>
+      <Title>Call me, man</Title>
     </HeaderSt>
-  )
-}
+  );
+};
